48985d7540b71cf424403bedec7a4fcd38aabd20
Jason Rowe <jason.rowe@protolabs.com>
2015-07-13 23:54:35 -0500

    add user path option

--- Options.cs
+++ Options.cs
@@ -2,66 +2,71 @@
 
 namespace AddPath
 {
-    public class Options
-    {
-        public EnvironmentVariableTarget Target { get; set; }
-        public string PathSegment { get; set; }
+	public class Options
+	{
+		public EnvironmentVariableTarget Target { get; set; }
+		public string PathSegment { get; set; }
 
-        public bool Parse(string[] args)
-        {
-            Target = EnvironmentVariableTarget.Machine;
+		public bool Parse(string[] args)
+		{
+			Target = EnvironmentVariableTarget.Machine;
 
-            foreach (string arg in args)
-            {
-                if (arg.StartsWith("-"))
-                {
-                    switch (arg)
-                    {
-                        case "-p":
-                            Target = EnvironmentVariableTarget.Process;
-                            break;
+			foreach (string arg in args)
+			{
+				if (arg.StartsWith("-"))
+				{
+					switch (arg)
+					{
+						case "-p":
+							Target = EnvironmentVariableTarget.Process;
+							break;
 
-                        case "-m":
-                            Target = EnvironmentVariableTarget.Machine;
-                            break;
+						case "-m":
+							Target = EnvironmentVariableTarget.Machine;
+							break;
 
-                        default:
-                            Console.WriteLine("Unknown option: {0}", arg);
-                            return false;
-                    }
-                }
-                else
-                {
-                    if (PathSegment == null)
-                    {
-                        PathSegment = arg;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Unknown parameter: {0}", arg);
-                        return false;
-                    }
-                }
+						case "-u":
+							Target = EnvironmentVariableTarget.User;
+							break;
 
-            }
+						default:
+							Console.WriteLine("Unknown option: {0}", arg);
+							return false;
+					}
+				}
+				else
+				{
+					if (PathSegment == null)
+					{
+						PathSegment = arg;
+					}
+					else
+					{
+						Console.WriteLine("Unknown parameter: {0}", arg);
+						return false;
+					}
+				}
 
-            if(PathSegment == null)
-            {
-                return false;
-            }
-            return true;
-        }
+			}
 
-        internal void Usage()
-        {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("    AddPath  [options] [path]");
-            Console.WriteLine();
-            Console.WriteLine("    [options] can be zero or more of:");
-            Console.WriteLine("        -p    only change path in process");
-            Console.WriteLine("        -m    change path on machine is default");
-            Console.WriteLine();
-            Console.WriteLine("    [path] directory you want to add");
-        }
-    }
+			if (PathSegment == null)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		internal void Usage()
+		{
+			Console.WriteLine("Usage:");
+			Console.WriteLine("    AddPath  [options] [path]");
+			Console.WriteLine();
+			Console.WriteLine("    [options] can be zero or more of:");
+			Console.WriteLine("        -p    only change path in process");
+			Console.WriteLine("        -m    only change path on machine is default");
+			Console.WriteLine("        -u    only change user path");
+			Console.WriteLine();
+			Console.WriteLine("    [path] directory you want to add");
+		}
+	}
 }
